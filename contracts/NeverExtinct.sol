d2951ca0a2553eea8edceeab0d2289a8ffeb1f8d
xKratos1901 <razors-clerics-0m@icloud.com>
2022-06-15 19:00:26 +0300

    resolved conflicts, wrote tests, updated smart contract

--- contracts/NeverExtinct.sol
+++ contracts/NeverExtinct.sol
@@ -22,12 +22,12 @@ contract NeverExtinct is ERC721Enumerable, Ownable {
     uint256 public limitPrivateSale = 1;
     uint256 public limitPublicSale = 1;
     uint256 public supply = 5000;
-    uint256 public privateSupply = 500;
+    uint256 public privateSupply = 5500;
     //? Visibility of these variables?
-    bool isPrivateSale = false;
-    bool isPublicSale = true; // Can we ahve both at the same time? if not, we can have 1 or the other.
-    bool isPaused = false;
-    bool isWhitelisted = false;
+    bool public isPrivateSale = true;  //true == privateSale ; false == publicSale
+    // bool public isPublicSale = true; // Can we ahve both at the same time? if not, we can have 1 or the other.
+    bool public isPaused = false;
+    bool public isWhitelisted = false;
 
     mapping(address => bool) public whitelistedAddresses;
     mapping(address => uint256) public mintedPerWallet;
@@ -35,6 +35,7 @@ contract NeverExtinct is ERC721Enumerable, Ownable {
     event WhitelistSingle(address user);
     event WhitelistMany(address[] users);
     event PublicMint(address[] users);
+    event WhitelistRemove(address user);
 
     constructor() ERC721("Never Extinct League", "NXT") {
         mintedPerWallet[msg.sender] = 0;
@@ -43,7 +44,7 @@ contract NeverExtinct is ERC721Enumerable, Ownable {
 
     function privateMint() public {
         require(!isPaused, "Contract it's paused");
-        require(isPrivateSale, "Private Sale Ended");
+        require(isPrivateSale, "Private sale ended");
         isWhitelisted = whitelistedAddresses[msg.sender];
         require(isWhitelisted, "Not Whitelisted");
         require(
@@ -55,12 +56,15 @@ contract NeverExtinct is ERC721Enumerable, Ownable {
         tokenId = _tokenId.current();
         tokenId;
         _safeMint(msg.sender, tokenId);
+        if(tokenId >= supply){
+            isPaused = true;
+        }
     }
 
     function publicMint() public {
         require(!isPaused, "Contract it's paused");
         require(tokenId < supply, "Supply exceeded");
-        require(isPublicSale, "Public Sale still waiting");
+        require(!isPrivateSale, "Public Sale still waiting");
         require(
             mintedPerWallet[msg.sender] < limitPublicSale,
             "Limit exceeded"
@@ -70,6 +74,9 @@ contract NeverExtinct is ERC721Enumerable, Ownable {
         tokenId = _tokenId.current();
         tokenId;
         _safeMint(msg.sender, tokenId);
+        if(tokenId >= supply){
+            isPaused = true;
+        }
     }
 
     function teamSupply(uint256 _amount) public onlyOwner {
@@ -104,6 +111,11 @@ contract NeverExtinct is ERC721Enumerable, Ownable {
         emit WhitelistSingle(_userWhitelist);
     }
 
+    function removeUser(address _userRemoved) public onlyOwner {
+        whitelistedAddresses[_userRemoved] = false;
+        emit WhitelistRemove(_userRemoved);
+    } // function to remove user from whitelist
+
     function setIsPaused(bool _state) public onlyOwner {
         isPaused = _state;
     }
@@ -112,9 +124,20 @@ contract NeverExtinct is ERC721Enumerable, Ownable {
         return isPaused;
     }
 
-    function publicSale() public onlyOwner {
-        isPrivateSale = false;
-        isPublicSale = true;
+    function changeSale(bool _state) public onlyOwner {
+        isPrivateSale = _state; //function changed to set true or false for private/publi sale
+    }
+
+    function changeLimitPrivat(uint256 _limit) public onlyOwner{
+        limitPrivateSale = _limit;
+    } //function for changing limit/wallet
+
+    function changeLimitPublic(uint256 _limits) public onlyOwner{
+        limitPublicSale = _limits;
+    } //function for changing limit/wallet
+
+    function getTokenId() public view returns(uint256) {
+        return tokenId;
     }
 
     function tokenURI(uint256 _tokenIds)
--- test/NeverExtinct.test.ts
+++ test/NeverExtinct.test.ts
@@ -6,13 +6,18 @@ import { NeverExtinct, NeverExtinct__factory } from "../typechain";
 
 describe("Never Extinct", function () {
   let owner: SignerWithAddress;
-  let user1: SignerWithAddress;
-  let user2: SignerWithAddress;
+  let user1: SignerWithAddress; //single user whitelist
+  let user2: SignerWithAddress; //batch whitelist
+  let user3: SignerWithAddress; //batch whitelist
+  let user4: SignerWithAddress; //batch whitelist
+  let user5: SignerWithAddress; //private mint
+  let user6: SignerWithAddress; //private mint
+  let ownerCopy : SignerWithAddress;
   let NeverExtinct: NeverExtinct;
 
   // so you don't need to do deploy for each test. just deploy once and set some variables above.
   beforeEach(async () => {
-    const [deployer, account1, account2] = await ethers.getSigners();
+    const [deployer, account1, account2, account3, account4, account5, account6,account7] = await ethers.getSigners();
     const NEArtifacts: NeverExtinct__factory = await ethers.getContractFactory(
       "NeverExtinct",
       owner
@@ -20,6 +25,11 @@ describe("Never Extinct", function () {
     owner = deployer;
     user1 = account1;
     user2 = account2;
+    user3 = account3;
+    user4 = account4;
+    user5 = account5;
+    user6 = account6;
+    ownerCopy = account7;
     NeverExtinct = await NEArtifacts.deploy();
   });
 
@@ -29,11 +39,81 @@ describe("Never Extinct", function () {
     });
   });
 
-  // describe("View functions", () => {
+  describe("Whitelist",() =>{
+    it("Add an address to whitelist", async function() {
+      await NeverExtinct.addUser(user1.address);
+      expect(await NeverExtinct.itsWhitelisted(user1.address)).to.be.true;
+    });
+    // it("Add an array of addresses to whitelist", async function(){
+    //   await NeverExtinct.batchWhitelist([user2.address,user3.address,user4.address]);
+    //   expect(await NeverExtinct.itsWhitelisted(user2.address)).to.be.true;
+    //   expect(await NeverExtinct.itsWhitelisted(user3.address)).to.be.true;
+    //   expect(await NeverExtinct.itsWhitelisted(user4.address)).to.be.true;
+    // });      This test seems not working
+    it("Remove user from whitelist", async function() {
+      await NeverExtinct.addUser(user4.address);
+      expect(await NeverExtinct.itsWhitelisted(user4.address)).to.be.true;
+      await NeverExtinct.removeUser(user4.address);
+      expect(await NeverExtinct.itsWhitelisted(user4.address)).to.be.false;
+    })
+  })
 
-  // }
+  describe("Private Minting", () => {
+    it("Add user to whitelist and mint",async function(){
+      await NeverExtinct.addUser(user5.address);
+      await NeverExtinct.connect(user5).privateMint();
+    });
+    // it("Add users and mint", async function(){
+    //   await NeverExtinct.batchWhitelist([user5.address,user6.address]);
+    //   await NeverExtinct.connect(user5).privateMint();
+    //   await NeverExtinct.connect(user6).privateMint();
+    // })   This Test as well
+    it("Try to mint without whitelist", async function(){
+      await expect(NeverExtinct.connect(user1).privateMint()).to.be.revertedWith('Not Whitelisted');
+    });
+    it("Mint 2 nft from same wallet", async function(){
+      await NeverExtinct.addUser(user1.address);
+      await NeverExtinct.connect(user1).privateMint();
+      await expect(NeverExtinct.connect(user1).privateMint()).to.be.revertedWith('Limit exceeded');
+    })
+  })
 
-  // describe("Write functions", () => {
+  describe("Public sale minting", () =>{
+    it("Stop privat minting and mint public", async function(){
+      await NeverExtinct.changeSale(false);
+      await NeverExtinct.connect(user2).publicMint();
+    });
+    it("Mint while privat minting it's still on", async function(){
+      await expect(NeverExtinct.connect(user2).publicMint()).to.be.revertedWith('Public Sale still waiting');
+    });
+    it("Mint 2 from same address", async function(){
+      await NeverExtinct.changeSale(false);
+      await NeverExtinct.connect(user2).publicMint();
+      await expect (NeverExtinct.connect(user2).publicMint()).to.be.revertedWith('Limit exceeded');
+    })
+  })
 
-  // }
+  describe("onlyOwner privileges", () =>{
+    it("Try to transfer ownership", async function(){
+      await expect(NeverExtinct.connect(ownerCopy).transferOwnership(user1.address)).to.be.revertedWith('Ownable: caller is not the owner');
+    });
+    it("Mint teamSupply", async function(){
+      await expect(NeverExtinct.connect(ownerCopy).teamSupply(5)).to.be.revertedWith('Ownable: caller is not the owner');
+    });
+    it("Change wallet limit privat", async function(){
+      await expect(NeverExtinct.connect(ownerCopy).changeLimitPrivat(2)).to.be.revertedWith('Ownable: caller is not the owner');
+    });
+    it("Change wallet limit public", async function(){
+      await expect(NeverExtinct.connect(ownerCopy).changeLimitPublic(2)).to.be.revertedWith('Ownable: caller is not the owner');
+    });
+    it("Make a safeTransfer",async function(){
+      await NeverExtinct.changeSale(false);
+      await NeverExtinct.connect(user1).publicMint();
+      await expect(NeverExtinct.connect(ownerCopy).transferFrom(user1.address,user2.address,1)).to.be.revertedWith('ERC721: transfer caller is not owner nor approved');
+    });
+    it("Pause the contract", async function(){
+      await expect(NeverExtinct.connect(ownerCopy).setIsPaused(true)).to.be.revertedWith('Ownable: caller is not the owner');
+    })
+  })
+
 });
